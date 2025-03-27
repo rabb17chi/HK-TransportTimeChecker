3c12c273f207faac4460c28e82f830e080895ad9
ichi <lala77tu@gmail.com>
2025-03-27 20:36:47 +0800

    end..?

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Home from '@/pages/home'
+import React from 'react'
+
+const page = () => {
+  return (
+    <Home />
+  )
+}
+
+export default page
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { useState } from "react";
 import MTR from "../../components/MTR";
 import KMB from "../../components/KMB";
