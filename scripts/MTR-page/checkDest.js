42ce72318f12a57c4f5e4fec83713d7f89aad262
ichi <lala77tu@gmail.com>
2025-03-31 08:20:28 +0800

    little change remove variable

--- scripts/MTR-page/checkDest.js
+++ scripts/MTR-page/checkDest.js
@@ -5,6 +5,5 @@ export default function checkDest(line,itemDest) {
     if (lineSummary[line].Up == itemDest || lineSummary[line].Down == itemDest) {
         return
     }
-    let diffDest = stationSummary[itemDest].name_tc
-    return `(${diffDest})`
+    return `(${stationSummary[itemDest].name_tc})`
 }
